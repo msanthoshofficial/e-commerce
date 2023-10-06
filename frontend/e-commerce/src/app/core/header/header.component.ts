667d2d8f87129fd8092b1eac462de1d11ced233f
Santhosh M <pmsanthosh2000@gmail.com>
2023-10-06 23:40:49 +0530

    nav menu init

--- frontend/e-commerce/src/app/core/header/header.component.ts
+++ frontend/e-commerce/src/app/core/header/header.component.ts
@@ -1,19 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AvatarModule } from 'primeng/avatar';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, AvatarModule],
+  imports: [CommonModule, AvatarModule, MenuModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
   profilePicture =
     'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png';
+  user: any = {};
+  items: any;
   ngOnInit() {
-    let user = JSON.parse(sessionStorage.getItem('user')!);
-    this.profilePicture = `data:${user.content_type};base64,${user.profile_picture}`;
+    this.user = JSON.parse(sessionStorage.getItem('user')!);
+    this.profilePicture = `data:${this.user.content_type};base64,${this.user.profile_picture}`;
+    this.items = [
+      {
+        label: 'Hi, ' + this.user.username,
+        items: [
+          {
+            label: 'Profile',
+            icon: 'pi pi-user',
+            command: () => {},
+          },
+        ],
+      },
+      {
+        label: 'Navigate',
+        items: [
+          {
+            label: 'Admin',
+            icon: 'pi pi-spin pi-cog',
+            url: 'http://angular.io',
+          },
+          {
+            label: 'Seller',
+            icon: 'pi pi-shopping-bag',
+            routerLink: '/fileupload',
+          },
+          {
+            label: '<span style="color:red">Logout</span>',
+
+            escape: false,
+            icon: 'pi pi-sign-out',
+            routerLink: '/fileupload',
+            iconClass: 'logout-icon',
+          },
+        ],
+      },
+    ];
   }
 }
