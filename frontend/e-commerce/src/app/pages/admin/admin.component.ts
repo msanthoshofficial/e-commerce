1e98ea796d74d3dd9ece613b830a212ac02c51ed
Santhosh M <pmsanthosh2000@gmail.com>
2023-10-07 18:57:19 +0530

    Admin component init

---
+++ frontend/e-commerce/src/app/pages/admin/admin.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-admin',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin.component.html',
+  styles: [
+  ]
+})
+export class AdminComponent {
+
+}
