398c857c271b4903959fcf7121a25a8d780c726b
Santhosh M <pmsanthosh2000@gmail.com>
2023-10-22 19:04:08 +0530

    Added Rate Limiting

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,7 @@ const routes = require("./routes/app.routes");
 const paymentController = require("./controllers/payment.controller");
 const compression = require("compression");
 const cookieParser = require("cookie-parser");
+const { rateLimit } = require("express-rate-limit");
 
 var app = express();
 app.use(compression());
@@ -16,6 +17,13 @@ app.use(cors({ origin: "http://localhost:4200", credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+const apiLimiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 1 minutes)
+	standardHeaders: "draft-7", // Set `RateLimit` and `RateLimit-Policy` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 app.use(
 	session({
@@ -35,6 +43,7 @@ connectDB().then(() => {
 	);
 	app.use(express.json());
 	app.use("/api", routes);
+	app.use("/api", apiLimiter);
 	app.get("/", (req, res) => {
 		res.send("Welcome to E-Commerce Backend");
 	});
