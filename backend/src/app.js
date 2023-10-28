fa14f30954b7791ebf398be4b3a34820c4f15b81
Santhosh M <pmsanthosh2000@gmail.com>
2023-10-28 15:23:08 +0530

    cors issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,15 @@ const { rateLimit } = require("express-rate-limit");
 var app = express();
 app.use(compression());
 
-app.use(cors({ origin: "http://localhost:4200", credentials: true }));
+app.use(
+	cors({
+		origin: [
+			"http://localhost:4200",
+			"https://e-commerce-app-silk.vercel.app",
+		],
+		credentials: true,
+	})
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
